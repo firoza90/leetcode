963c1dc396e0f14f03c19a8739dd8816d185ff1d
firoza90 <53232581+firoza90@users.noreply.github.com>
2020-10-09 04:19:32 +0530

    module to remove given value in-place

---
+++ remove_value_inplace.py
@@ -0,0 +1,23 @@
+
+"""
+https://leetcode.com/problems/remove-element/
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+"""
+
+def removeElement( nums, val):
+    """
+    :type nums: List[int]
+    :type val: int
+    :rtype: int
+    """
+    w_index = 0
+
+    for r_index in range(0, len(nums)):
+        if val != nums[r_index]:
+            nums[w_index] = nums[r_index]
+            w_index += 1
+    return w_index
+
+print(removeElement([0,1,2,2,3,0,4,2],2))
