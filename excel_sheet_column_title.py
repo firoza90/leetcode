7d351edb11e53f46bd7517a8f365405b0fdfb61b
firoza90 <firoza.mariam.c@gmail.com>
2021-05-29 15:38:13 +0530

    cleanup mapping number to excel column title

--- excel_sheet_column_title.py
+++ excel_sheet_column_title.py
@@ -16,14 +16,14 @@ AB -> 28
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         import string
-        colMapping = dict([map for map in enumerate(string.ascii_uppercase, start = 1)])
+        colMapping = dict([map for map in enumerate(string.ascii_uppercase, start = 0)])
         LENGTH = 26
         result = ""
-        while columnNumber > LENGTH:
+        while columnNumber > 0:
+            columnNumber = columnNumber - 1
             remainder = columnNumber % LENGTH
-            result = result + colMapping[LENGTH if remainder == 0 else remainder]
-            columnNumber = columnNumber // LENGTH if remainder != 0 else columnNumber // LENGTH  -1
-        result = result + colMapping[columnNumber]
+            result = result + colMapping[remainder]
+            columnNumber = columnNumber // LENGTH
         return result[::-1]
 
     def test(self):
